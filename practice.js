e8c8880c0babb76ecc2b44c1aa3ab0cf99f1c524
Constantine <142466271+GORWhitewolF@users.noreply.github.com>
2025-05-18 22:09:50 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,33 @@
+// var a=23
+// var b=22
+// function add(){
+//     console.log(a+b)
+// }
+
+// function area(length,breadth){
+//     console.log("Area is "+length*breadth)
+// }
+
+// area(30,40)
+// var a=21
+
+// if(a%2==0){
+//     console.log("your number is even")
+// }
+// else{
+//     console.log("your number is odd")
+// }
+ 
+// var b=["a","e","i","o","u"]
+// var c="q"
+// if(b.includes(c)){
+//     console.log("Its a vowel"
+//     )
+// }
+// else{
+//     console.log("consonant")
+// }
+
+for( i=1;i<6;i=i+2){
+console.log(i)
+}
